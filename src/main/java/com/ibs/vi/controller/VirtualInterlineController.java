b1591446df228fd237cf12bb4f9f83d776e2a555
jithin <jithinpjose12@gmail.com>
2025-05-26 09:34:19 +0530

    INitial commit

--- src/main/java/com/ibs/vi/controller/VirtualInterlineController.java
+++ src/main/java/com/ibs/vi/controller/VirtualInterlineController.java
@@ -1,8 +1,11 @@
 package com.ibs.vi.controller;
 
+import com.ibs.vi.model.VirtualInterlineDataModel;
 import com.ibs.vi.service.VirtualInterlineService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,13 @@ public class VirtualInterlineController {
     public String checkHealth() {
         return virtualInterlineService.getHealthStatus();
     }
+    
+   @GetMapping("/flight/search")
+    public List<List<VirtualInterlineDataModel>> flightSearch(
+            @RequestParam(value = "source", required = true) String source, 
+            @RequestParam(value = "destination", required = true) String destination,
+            @RequestParam(value = "date", required = true) String date) throws Exception {
+        return virtualInterlineService.fetchFlightDetails(source, destination, date);
+    } 
 
 }
