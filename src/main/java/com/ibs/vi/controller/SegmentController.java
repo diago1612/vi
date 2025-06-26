6f0b1b0e04e25f2de49ed4e0ea0ba93d604c1137
Swathi Mukundan <A-10681@LE2063.local>
2025-06-26 11:05:03 +0530

    feat:redis save

--- src/main/java/com/ibs/vi/controller/SegmentController.java
+++ src/main/java/com/ibs/vi/controller/SegmentController.java
@@ -5,6 +5,7 @@ import com.ibs.vi.model.Segment;
 import com.ibs.vi.service.RouteService;
 import com.ibs.vi.view.AirlineView;
 import com.ibs.vi.view.BasicResponseView;
+import com.ibs.vi.view.SegmentView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -14,17 +15,37 @@ import java.util.List;
 @RestController
 @RequestMapping("segments")
 public class SegmentController {
+
     @Autowired
     @Qualifier("segmentManagementService")
     private RouteService routeService;
 
     @PostMapping
-    public BasicResponseView create(@RequestBody Segment segment) {
+    public BasicResponseView createSegment(@RequestBody Segment segment) {
         return routeService.save(segment);
     }
 
+    @GetMapping("{key}")
+    public SegmentView getSegmentByKey(@PathVariable String key, @RequestParam String airline) {
+        return (SegmentView) routeService.getByKey(key,airline);
+    }
+
     @GetMapping
-    public List<AirlineView> getAllRoutes() {
+    public List<SegmentView> getAllSegments() {
         return routeService.getAll();
+    }
+
+    @PutMapping("{key}")
+    public SegmentView updateSegmentByKey(@PathVariable String key, @RequestBody Segment segment) {
+        return (SegmentView)routeService.updateByKey(key, segment);
+    }
+
+    @DeleteMapping("{key}")
+    public BasicResponseView deleteSegmentByKey(@PathVariable String key, @RequestParam String airline) {
+        return routeService.deleteByKey(key, airline);
+    }
+    @DeleteMapping("delete/all")
+    public BasicResponseView deleteSegments(@RequestParam String airline) {
+        return routeService.deleteAll();
     }
 }
